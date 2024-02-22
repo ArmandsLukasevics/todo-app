4ae7403572d476639b62c6b3e35364a9cc5b5f74
ArmandsLukasevics <armands.lukasevics@magebit.com>
2024-02-22 15:08:56 +0200

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app-container">
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  created() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.todos = JSON.parse(storedTodos);
+    }
+  },
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj];
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
